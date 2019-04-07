5af29e91a688fe40b66be50aa107991c66f878ff
An Le <lehoangthienan@gmail.com>
2019-04-07 20:05:46 +0700

    Add meta seo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Helmet} from "react-helmet";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+                <meta charSet="utf-8" />
+                <title>An Le</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
         <h1>Googlebot will always crawl</h1>
       </div>
     );
