2d272f3d17a59808495fc2fd56b6223f4effef11
An Le <lehoangthienan@gmail.com>
2019-04-07 22:34:27 +0700

    Test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <meta charSet="utf-8" />
+        {/* <meta charSet="utf-8" />
         <title>An Le</title>
         <link rel="canonical" href="http://mysite.com/example" />
         <meta name="description" content="An Le 1" />
@@ -18,7 +18,20 @@ class App extends Component {
                 <meta name="description" content="An Le" />
               <meta property="og:title" content="An Le" />
               <meta property="og:image" content="https://scontent.fsgn5-7.fna.fbcdn.net/v/t1.0-9/52450069_1244416252350097_4263800870111543296_o.jpg?_nc_cat=103&_nc_oc=AQmc0uC5YEtUj4Q26jR0zAlxjcK4s86rUyMFKoZqacxZ05ZCLBBjNv5pTnJD1SwknjH24axpjnqqDosZihwv-26y&_nc_ht=scontent.fsgn5-7.fna&oh=fd1da05c9b797a8fcb67189d8c5b0e78&oe=5D48EA49" />
-            </Helmet>
+            </Helmet> */}
+            <title>VEED - Create, Edit & Share Videos Online for free</title>
+    <meta name="description" content="Veed is a simple but powerfull video editor, try our free video editor to, resize video, trim video add loads more!" />
+    <meta name="keywords" content="instgram video editor, Online Video editor, Video Editor, Video Resizer, Trim Video, Draw On Video, Resize Video, Edit videos online, Online video editor" />
+    <meta name="application-name" content="VEED" />
+    <meta name="theme-color" content="#e162f8" />
+
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:url" content="https://www.veed.io/" />
+    <meta property="og:title" content="VEED - Create, Edit & Share Videos Online for free" />
+    <meta property="og:description" content="Veed is a simple but powerfull video editor, try our free video editor to, resize video, trim video add loads more!" />
+    <meta property="og:image" content="https://www.veed.io/fav/og-image.png" />
+    <meta property="og:image:url" content="https://www.veed.io/fav/og-image.png" />
         <h1>Googlebot will always crawl</h1>
       </div>
     );
